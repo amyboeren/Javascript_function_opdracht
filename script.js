271e3b7630395a3423f36a2b8797839be1a8e48e
AmyB <31877717+amyboeren@users.noreply.github.com>
2021-10-26 20:08:00 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+const receptPizza = function () {
+  console.log("Kneed het deeg")
+  console.log("Maak een ronde pizzavorm")
+  console.log("Beleg met pizzasaus")
+  console.log("Beleg met mozzarella en tomaten")
+  console.log("Bestrooi met geraspte kaas")
+  console.log("Bak de pizza 12 minuten in de oven op 220 graden")
+};
+
+const receptSushi = function () {
+  console.log("Kook de witte rijst")
+  console.log("Snijd de reepjes zalm")
+  console.log("Snijd de komommer en vouw deze in de rijst")
+  console.log("Maak rolletjes van de rijst")
+  console.log("Vouw de zalm rond de rolletjes rijst")
+};
+receptSushi();
+receptSushi();
+receptSushi();
+receptPizza();
+receptPizza();
